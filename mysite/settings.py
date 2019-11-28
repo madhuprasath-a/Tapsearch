006f74d35ff164e0691d3ce74d36fe41323f1825
madhumsd7 <madhupasath.angurajhicas@gmail.com>
2019-11-28 11:48:27 +0530

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mxn)!)aw-tx(@t^3xy@myn5nq*4w14h#_jxlk*@xp(xa&xyad3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fast-everglades-86279.herokuapp.com']
+ALLOWED_HOSTS = ['invertedsearch.herokuapp.com']
 
 
 # Application definition
@@ -119,8 +119,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/statics/'
+#Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+#Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
+
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
